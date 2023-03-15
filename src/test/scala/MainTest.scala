140c5214fe0885c5d0420e79bb205e91f6c9e334
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2023-03-15 10:45:02 +0100

    Effects working, not propagating failures See https://github.com/typelevel/scalacheck-effect/issues/261

--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -1,7 +1,10 @@
 import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalacheck._
+import org.scalacheck.effect.PropF
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+
+import scala.concurrent.Future
 
 class MainTest extends AsyncFlatSpec with Matchers with ScalaCheckPropertyChecks {
   behavior of "Main"
@@ -11,4 +14,16 @@ class MainTest extends AsyncFlatSpec with Matchers with ScalaCheckPropertyChecks
       assert(i < 5) // intentional fail - we want to see how the failure is reported
     }
   }
+
+  it should "perform async computations" in {
+
+    PropF.forAllF(Gen.oneOf(0 until 10)) { i  =>
+      Future {
+        assert(i < 5) // intentional fail - we want to see how the failure is reported
+      }.map(_ => ()) // forAllF supports Future[Unit] by default, so we need to throw away the Assertion value
+    }.check().map { r =>
+      assert(r.passed) // works, but does not propagate the test result
+    }
+  }
+
 }
